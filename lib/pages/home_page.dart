449588d666ea33e87aae9e195191d793a88ff958
Saeed Mohammad Sabra <saeedsabra565@gmail.com>
2023-11-17 13:13:24 +0200

    implement chat feature and user list, add cloud_firestore dependency, and register cloud_firestore plugin

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
-import 'package:chat_app/pages/login_page.dart';
+import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/services/auth/auth_service.dart';
-import 'package:chat_app/services/auth/login_or_register.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,6 +13,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // instance of auth
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //sign user out
   void signOut() {
     final authService = Provider.of<AuthService>(context, listen: false);
 
@@ -30,6 +35,54 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      body: _buildUserList(),
     );
+  }
+
+  //build a list of users except for the current logged in user
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("users").snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading...");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs
+              .map<Widget>((doc) => _buildUserListItem(doc))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildUserListItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+    //display all user except current user
+    if (_auth.currentUser!.email != data['email']) {
+      return ListTile(
+        title: Text(data['email']),
+        onTap: () {
+          //pass the clicked users UID to the chat page
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ChatPage(
+                receiverUserEmail: data["email"],
+                receiverUserID: data["uid"],
+              ),
+            ),
+          );
+        },
+      );
+    } else {
+      return Container();
+    }
   }
 }
--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,9 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthService extends ChangeNotifier {
+  //instance of auth
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
+  // instance of firestore
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
+
+  //Get the current user
   Future<UserCredential> signInWithEmailandPassword(
       String email, String password) async {
     try {
@@ -13,12 +19,20 @@ class AuthService extends ChangeNotifier {
         password: password,
       );
 
+      // add a new document for the user in users collection if it doesn't already exists
+      _fireStore.collection("users").doc(userCredential.user!.uid).set({
+        "uid": userCredential.user!.uid,
+        "email": email,
+        "createdAt": FieldValue.serverTimestamp(),
+      }, SetOptions(merge: true));
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
     }
   }
 
+  //Create a new user
   Future<UserCredential> signUpWithEmailandPassword(
       String email, String password) async {
     try {
@@ -28,12 +42,19 @@ class AuthService extends ChangeNotifier {
         password: password,
       );
 
+      _fireStore.collection("users").doc(userCredential.user!.uid).set({
+        "uid": userCredential.user!.uid,
+        "email": email,
+        "createdAt": FieldValue.serverTimestamp(),
+      });
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
     }
   }
 
+  //Sign out
   Future<void> signOut() async {
     return await FirebaseAuth.instance.signOut();
   }
